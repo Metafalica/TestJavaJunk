1455e88087ba052411c227853bfa8efd5b6e93a6
Kosmo零 <metafalica@gmx.com>
2026-08-14 12:13:17 +0400

    Исправления по комментарию к пулл реквесту.

--- TestJavaJunk/src/main/java/com/zerolabs/testjavajunk/GeneralClass.java
+++ TestJavaJunk/src/main/java/com/zerolabs/testjavajunk/GeneralClass.java
@@ -1,36 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.zerolabs.testjavajunk;
 
-/**
- *
- * @author Infel Phira
- */
-public final class GeneralClass implements Cloneable
-{
+public final class GeneralClass implements Cloneable {
     private int generalIntegerValue;
     
-    public int getGeneralIntegerValue() { return this.generalIntegerValue; }
-    public void setGeneralIntegerValue(int value) { this.generalIntegerValue = value; }
+    public int getGeneralIntegerValue() {
+        return this.generalIntegerValue;
+    }
+    public void setGeneralIntegerValue(int value) {
+        this.generalIntegerValue = value;
+    }
     
-    public GeneralClass(int generalIntegerValue)
-    {
+    public GeneralClass(int generalIntegerValue) {
         setGeneralIntegerValue(generalIntegerValue);
     }
     
     @Override
-    public GeneralClass clone()
-    {
+    public GeneralClass clone() {
         GeneralClass newGC = null;
         
-        try
-        {
+        try {
             newGC = (GeneralClass)super.clone();
         }
-        catch (CloneNotSupportedException e)
-        {
+        catch (CloneNotSupportedException e) {
             int currGenIntVal = getGeneralIntegerValue();
             newGC = new GeneralClass(currGenIntVal);            
         }
